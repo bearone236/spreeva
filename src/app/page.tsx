c86febf916101a735a8e0f11013983bc8615b311
bearone236 <kuma_2306@icloud.com>
2024-09-13 15:34:50 +0900

    feat: create select-pagination

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,9 @@ export default function Home() {
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
       <h2>Hello Kazuya</h2>
       <Button>
+        <Link href={'/select'}>Select</Link>
+      </Button>
+      <Button>
         <Link href={'/login'}>Login</Link>
       </Button>
     </main>
