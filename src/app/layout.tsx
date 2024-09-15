be493cdc1c6b8ea98b6d0806bde75a04f46670c1
bearone236 <kuma_2306@icloud.com>
2024-09-15 17:28:08 +0900

    feat: created thinking-pagination

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
     <html lang='en'>
       <body className={inter.className}>
         <Header />
-        {children}
+        <div className='mt-5'>{children}</div>
       </body>
     </html>
   )
