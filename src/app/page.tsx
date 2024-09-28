b4d1e106d4c31c9c8f978f33e6bc305943eceeec
bearone236 <kuma_2306@icloud.com>
2024-09-28 14:41:57 +0900

    home画面の表示方法の変更

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,48 @@
-'use client'
-
 import ContributionGraph from '@/components/ContributionGraph'
 import GamificationCard from '@/components/GemificationCard'
 import QuickStartForm from '@/components/QuickStartForm'
-import { useSession } from 'next-auth/react'
+import { auth } from './api/auth/[...nextauth]/auth';
 
-export default function Page() {
-  const { data: session } = useSession()
+export default async function Page() {
+  const session = await auth(); 
 
   return (
     <>
       <main className='min-h-screen'>
-        <h2>
-          {session?.user?.name ? `Hello, ${session.user.name}` : 'Hello, Guest'}
-        </h2>
-        <div className='flex flex-col md:flex-row gap-6 mt-5'>
-          <div className='md:w-1/2'>
+
+
+        {session?.user ? (
+          <>
+            <h2 className="text-2xl font-bold">
+              {`Hello, ${session.user.name}`}
+            </h2>
+            <div className='flex flex-col'>
+              <div className='flex flex-col md:flex-row gap-6 mt-5'>
+                <div className='md:w-1/2'>
+                  <QuickStartForm />
+                </div>
+                <div className='md:w-1/2'>
+                  <GamificationCard
+                    streak={7}
+                    level={5}
+                    points={78}
+                    nextReward={10}
+                  />
+                </div>
+              </div>
+              <div className='mt-9'>
+                <ContributionGraph />
+              </div>
+            </div>
+          </>
+        ) : (
+            <div className='mt-5 w-[800px] mx-auto'>
+              <h2 className="text-2xl font-bold pb-7">
+              Hello, Guest
+            </h2>
             <QuickStartForm />
           </div>
-          <div className='md:w-1/2'>
-            <GamificationCard
-              streak={7}
-              level={5}
-              points={78}
-              nextReward={10}
-            />
-          </div>
-        </div>
-        <div className='mt-9'>
-          <ContributionGraph />
-        </div>
+        )}
       </main>
     </>
   )
