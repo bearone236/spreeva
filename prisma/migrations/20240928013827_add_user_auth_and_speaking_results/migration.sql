7eef36567941b8829d8aa856a9807bb35f2a8421
bearone236 <kuma_2306@icloud.com>
2024-09-28 11:46:07 +0900

    Updated Prisma Schema

---
+++ prisma/migrations/20240928013827_add_user_auth_and_speaking_results/migration.sql
@@ -0,0 +1,29 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `avatarUrl` on the `User` table. All the data in the column will be lost.
+  - You are about to drop the column `googleId` on the `User` table. All the data in the column will be lost.
+  - You are about to drop the `VerificationToken` table. If the table is not empty, all the data it contains will be lost.
+  - Added the required column `updatedAt` to the `Account` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `updatedAt` to the `Session` table without a default value. This is not possible if the table is not empty.
+  - Made the column `email` on table `User` required. This step will fail if there are existing NULL values in that column.
+
+*/
+-- DropIndex
+DROP INDEX "User_googleId_key";
+
+-- AlterTable
+ALTER TABLE "Account" ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL;
+
+-- AlterTable
+ALTER TABLE "Session" ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL;
+
+-- AlterTable
+ALTER TABLE "User" DROP COLUMN "avatarUrl",
+DROP COLUMN "googleId",
+ALTER COLUMN "email" SET NOT NULL;
+
+-- DropTable
+DROP TABLE "VerificationToken";
