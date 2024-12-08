b98ece7603c5b45649340c1d51bb4a45e2bbce59
bearone236 <kuma_2306@icloud.com>
2024-12-08 23:56:45 +0900

    HeaderのUIを修正

--- src/components/Header/HeaderUserMenu.tsx
+++ src/components/Header/HeaderUserMenu.tsx
@@ -2,22 +2,24 @@
 
 import { User } from 'lucide-react'
 import type { Session } from 'next-auth'
-import { signOut } from 'next-auth/react'
 import Image from 'next/image'
-import { Button } from '../ui/button'
 
 export default function HeaderUserMenu({ session }: { session: Session }) {
   return (
     <>
       {session.user.image ? (
-        <Image
-          src={session.user.image}
-          alt='User Profile'
-          width={40}
-          height={40}
-          className='rounded-full'
-          priority
-        />
+        <>
+          <Image
+            src={session.user.image}
+            alt='User Profile'
+            width={40}
+            height={40}
+            className='rounded-full'
+            priority
+          />
+
+          <p className='text-white'>{session.user.name}</p>
+        </>
       ) : session.user.userType ? (
         <div className='flex items-center text-white'>
           <User />
@@ -28,13 +30,6 @@ export default function HeaderUserMenu({ session }: { session: Session }) {
       ) : (
         <div className='text-white'>プロフィール未設定</div>
       )}
-      <Button
-        variant='ghost'
-        className='text-white hover:bg-white hover:bg-opacity-90 transition-colors flex items-center border border-white'
-        onClick={() => signOut({ callbackUrl: '/' })}
-      >
-        ログアウト
-      </Button>
     </>
   )
 }
