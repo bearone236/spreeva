dac2f5e7258409ef97940d13017ee7396b34284b
bearone236 <kuma_2306@icloud.com>
2024-12-10 15:05:42 +0900

    Prismaスキーマの修正 / 不要なテーブル・リレーションの削除

---
+++ prisma/migrations/20241210030643_delete_evaluation_ai_improved_text/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `aiImprovedText` on the `Evaluation` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Evaluation" DROP COLUMN "aiImprovedText";
