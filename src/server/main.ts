0ea6e22398a0748b5438ed2fa09c8c7e2370bdde
bearone236 <kuma_2306@icloud.com>
2024-12-25 23:39:56 +0900

    main.tsのコード整理

--- src/server/main.ts
+++ src/server/main.ts
@@ -3,46 +3,60 @@ import { Hono } from 'hono'
 import { hc } from 'hono/client'
 import { cors } from 'hono/cors'
 import { logger } from 'hono/logger'
+
+// Controllers import
 import { SpeakingEvaluationController } from './controllers/SpeakingEvaluationController'
 import { SpeechController } from './controllers/speechController'
 import { ThemeController } from './controllers/themeController'
+
+// Repositories import
 import { GeminiEvaluationRepository } from './repository/GeminiEvaluationRepository'
 import { GeminiThemeRepository } from './repository/GeminiThemeRepository'
 import { GoogleVisionOCRRepository } from './repository/GoogleVisionOCRRepository'
 import { SpeechRepository } from './repository/speechRepository'
+
+// Use Cases import
 import { EvaluateSpeakingUseCase } from './usecase/EvaluateSpeakingUseCase'
 import { ThemeUseCase } from './usecase/ThemeUseCase'
 import { PDFThemeGenerationUsecase } from './usecase/pdfThemeGeneration'
 import { RecognizeSpeechUsecase } from './usecase/recognizeSpeech'
 
+// 環境変数
 const GEMINI_API_URL = process.env.GEMINI_API_URL as string
 const GEMINI_API_KEY = process.env.GEMINI_API_KEY as string
-
-const prisma = new PrismaClient()
+const VERCEL_URL = process.env.VERCEL_URL as string
 
 if (!GEMINI_API_URL || !GEMINI_API_KEY) {
   throw new Error('GEMINI_API_URL or GEMINI_API_KEY is not defined')
 }
 
+if (!VERCEL_URL) {
+  throw new Error('VERCEL_URL is not defined')
+}
+
+// Prisma client
+const prisma = new PrismaClient()
+
+// Repository instances
 const themeRepository = new GeminiThemeRepository(
   GEMINI_API_URL,
   GEMINI_API_KEY,
 )
-
 const evaluationRepository = new GeminiEvaluationRepository(prisma)
+const googleVisionOCRRepository = new GoogleVisionOCRRepository()
+const speechRepository = new SpeechRepository()
 
+// Use Case instances
 const generateThemeUseCase = new ThemeUseCase(themeRepository)
 const evaluateSpeakingUseCase = new EvaluateSpeakingUseCase(
   evaluationRepository,
 )
-
-const googleVisionOCRRepository = new GoogleVisionOCRRepository()
 const pdfThemeGenerationUsecase = new PDFThemeGenerationUsecase(
   googleVisionOCRRepository,
 )
+const recognizeSpeechUsecase = new RecognizeSpeechUsecase(speechRepository)
 
-const app = new Hono().basePath('/api')
-
+// Controller instances
 const themeController = new ThemeController(
   generateThemeUseCase,
   pdfThemeGenerationUsecase,
@@ -50,24 +64,20 @@ const themeController = new ThemeController(
 const evaluationController = new SpeakingEvaluationController(
   evaluateSpeakingUseCase,
 )
-const speechRepository = new SpeechRepository()
-const recognizeSpeechUsecase = new RecognizeSpeechUsecase(speechRepository)
 const speechController = new SpeechController(recognizeSpeechUsecase)
 
+// Hono app
+const app = new Hono().basePath('/api')
+
+// Middlewares
 app.use('*', cors())
 app.use(logger())
 
+// Routes
 app.post('/theme', c => themeController.handleThemeGeneration(c))
 app.post('/evaluate', c => evaluationController.handleEvaluation(c))
 app.post('/speech', c => speechController.handleRecognizeSpeech(c))
 app.post('/upload-pdf', c => themeController.handleThemeGeneration(c))
 
-type AppType = typeof app
-
-if (!process.env.VERCEL_URL) {
-  throw new Error('VERCEL_URL is not defined')
-}
-
-export const rpcClient = hc<AppType>(process.env.VERCEL_URL)
-
+export const rpcClient = hc<typeof app>(VERCEL_URL)
 export default app
